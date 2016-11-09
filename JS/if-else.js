a45bff32241c6ca3c7db72f52a93d92d26dcde64
Pierre Irby <stoked49@yahoo.com>
2016-11-09 14:12:47 -0600

    9Nov16 Update 1.

--- JS/if-else.js
+++ JS/if-else.js
@@ -7,51 +7,82 @@
 
 // Exercise 3a
 
-var i = ((70+80+95)/3);
-if (i >= 80){
-    console.log("You're Awesome");
-} else {
-    console.log("You need to practice more");
+// var i = ((70+80+95)/3);
+// if (i >= 80){
+//     console.log("You're Awesome");
+// } else {
+//     console.log("You need to practice more");
+// }
+//
+//
+//
+// // Exercise 3b
+//
+// var cam = 180;
+// var discount = 10;
+// if (cam >= 200) {
+//     var camfinal = cam - discount;
+//     console.log("Cameron you have earned a discount of $" + discount + " and your total is $" + camfinal);
+// } else {
+//     console.log("Cameron your total is $" + cam);
+// }
+//
+// var ryan = 250;
+// var discount = 10;
+// if (ryan >= 200) {
+//     var ryanfinal = ryan - discount;
+//     console.log("Ryan you have earned a discount of $" + discount + " and your total is $" + ryanfinal);
+// } else {
+//     console.log("Ryan your total is $" + ryan);
+// }
+//
+// var geo = 180;
+// var discount = 10;
+// if (geo >= 200) {
+//     var geofinal = geo - discount;
+//     console.log("George you have earned a discount of $" + discount + " and your total is $" + geofinal);
+// } else {
+//     console.log("George your total is $" + geo);
+// }
+//
+//
+//
+// // Exercise 3c
+//
+// var flipACoin = Math.floor(Math.random()* 2);
+// if (flipACoin == 0) {
+//     console.log("Buy a car");
+// } else {
+//     console.log("Buy a house");
+// }6
+
+
+var discount = .25;
+var displaydiscount = 25;
+
+function getShopperName() {
+    return prompt("What is your name?");
+}
+function getGroceryTotal() {
+    return prompt("What is the total of your groceries?");
+}
+
+var shopperName = getShopperName();
+var groceryTotal = getGroceryTotal();
+
+function calculateHEBDiscount(shopperName, groceryTotal, discount) {
+    var message;
+    var finalCost = (groceryTotal - (groceryTotal*discount));
+    if (groceryTotal >= 200) {
+        message = shopperName + " you have earned a discount of " + displaydiscount + "% and your final cost is $" + finalCost;
+    } else {
+        message = shopperName + ", you have not qualified for a discount and your remaining total is $" + groceryTotal;
+    }
+    return message
 }
 
 
+var result = calculateHEBDiscount(shopperName, groceryTotal, discount);
+alert (result);
+console.log(result);
 
-// Exercise 3b
-
-var cam = 180;
-var discount = 10;
-if (cam >= 200) {
-    var camfinal = cam - discount;
-    console.log("Cameron you have earned a discount of $" + discount + " and your total is $" + camfinal);
-} else {
-    console.log("Cameron your total is $" + cam);
-}
-
-var ryan = 250;
-var discount = 10;
-if (ryan >= 200) {
-    var ryanfinal = ryan - discount;
-    console.log("Ryan you have earned a discount of $" + discount + " and your total is $" + ryanfinal);
-} else {
-    console.log("Ryan your total is $" + ryan);
-}
-
-var geo = 180;
-var discount = 10;
-if (geo >= 200) {
-    var geofinal = geo - discount;
-    console.log("George you have earned a discount of $" + discount + " and your total is $" + geofinal);
-} else {
-    console.log("George your total is $" + geo);
-}
-
-
-
-// Exercise 3c
-
-var flipACoin = Math.floor(Math.random()* 2);
-if (flipACoin == 0) {
-    console.log("Buy a car");
-} else {
-    console.log("Buy a house");
-}
---
+++ JS/iterating.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Irby on 11/9/16.
+ */
+
+(function(){
+    "use strict";
+
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+
+    var names = ["Papo", "Melanie", "Brian", "Yassine"];
+
+    // TODO: Create a log statement that will log the number of elements in the names array.
+
+    console.log("there are " + names.length + " names in the array");
+
+    // TODO: Create log statements that will print each of the names array elements individually.
+    console.log("The first name is " + names[0]);
+    console.log("The second name is " + names[1]);
+    console.log("The third name is " + names[2]);
+    console.log("The forth name is " + names[3]);
+
+})();
